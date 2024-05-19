f460afc54e1a1d6b29fa4dddd8ac6bf31288e834
Devesh Mehta <devesh1217@gmail.com>
2024-05-19 18:44:36 +0530

    Basic React App with Tailwind css

---
+++ src/Components/Board.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../Static/output.css"
+
+function Board() {
+    return (
+        <div className="flex justify-center p-24">Board</div>
+    )
+}
+
+export default Board;
